aebcd0cf1b7d2857cc3d88e84ad49b2783da3bc8
Mateus_Fattori <mffattori@gmail.com>
2022-10-05 13:40:19 -0300

    .

--- src/app/cadastro/cadastro.page.ts
+++ src/app/cadastro/cadastro.page.ts
@@ -1,5 +1,9 @@
+/* eslint-disable @typescript-eslint/dot-notation */
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Cadastro } from '../models/Cadastro';
+import { AlertController } from '@ionic/angular';
+import { CadastroService } from '../services/cadastro';
 
 @Component({
   selector: 'app-cadastro',
@@ -8,17 +12,57 @@ import { Router } from '@angular/router';
 })
 export class CadastroPage implements OnInit {
 
-  constructor(private rota: Router) { }
+  public listaCadastros: Cadastro[] = [];
+
+  constructor(private rota: Router, private cadaService: CadastroService, private alertController: AlertController) { }
 
   abrirPaginaLogin(){
-    this.rota.navigateByUrl(`/log-in`)
+    this.rota.navigateByUrl(`/log-in`);
   }
 
   abrirPaginaHome(){
-    this.rota.navigateByUrl(`/tabs/home`)
+    this.rota.navigateByUrl(`/tabs/home`);
   }
 
   ngOnInit() {
+  }
+
+  // botão buscar sem utilidade por enquanto
+
+  buscarCadastros() {
+    this.cadaService.buscarCadastro().subscribe(dadosRetorno => {
+      this.listaCadastros = dadosRetorno.map(registro => (
+        {
+          id: registro.payload.doc.id,
+          nome: registro.payload.doc.data()['nome'],
+          senha: registro.payload.doc.data()['senha']
+        }
+      ));
+    });
+  }
+
+  async deletarCadastro(id: string) {
+    const alert = await this.alertController.create({
+      header: 'Confirma exclução dessa conta',
+      buttons: [
+        {
+          text: 'Não',
+          role: 'Cancel',
+          handler: () => {
+
+            },
+          },
+          {
+            text: 'Sim',
+            role: 'confirmar',
+            handler: () => {
+              this.cadaService.deletar(id);
+          },
+        },
+      ],
+    });
+    await alert.present();
+    this.buscarCadastros();
   }
 
 }
