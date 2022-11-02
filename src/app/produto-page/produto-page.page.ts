904dc3bb86a0423d030640872e7653435217f23d
João Rosinhole <jvr131104@gmail.com>
2022-11-02 09:34:40 -0300

    ACABOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO AEEEEEEEEEEEEEEEEEEEE

--- src/app/produto-page/produto-page.page.ts
+++ src/app/produto-page/produto-page.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { BehaviorSubject } from 'rxjs';
+import { ModalController } from '@ionic/angular';
+import { CartService } from 'src/app/services/cart.service';
+
 
 @Component({
   selector: 'app-produto-page',
@@ -7,14 +12,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProdutoPagePage implements OnInit {
 
-  favoritado = true;
-  constructor() { }
+
+  cart = [];
+  products = [];
+  cartItemCount: BehaviorSubject<number>;
+
+  private currentNumber = 0;
+
+  constructor(private rota: Router, private cartService: CartService, private modalCtrl: ModalController) { }
 
   ngOnInit() {
   }
 
-  favoritar(): void {
-    this.favoritado = !this.favoritado;
+  private mais() {
+    this.currentNumber++;
+  }
+  
+  private menos() {
+    this.currentNumber--;
+  }
+
+  addToCart(products) {
+    this.cartService.addProduct(products);
+  }
+
+  abrirPaginaCarrinho(){
+    this.rota.navigateByUrl(`/carrinho`);
+  }
+
+  voltar(){
+    this.rota.navigateByUrl(`/tabs/home`);
   }
 
 }
